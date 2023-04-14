f35979759fc5f74f2e9a2f3f4b9a368450f86e00
Jelmar Orapa <53886894+orapagier@users.noreply.github.com>
2023-04-14 14:05:02 +0800

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,49 +1,17 @@
-import { useState } from "react"
-
-export function TodoItem({ completed, id, title, toggleTodo, deleteTodo, editTodo }) {
-  const [editing, setEditing] = useState(false)
-  const [newTitle, setNewTitle] = useState(title)
-
-  function handleSave() {
-    editTodo(id, newTitle)
-    setEditing(false)
-  }
-
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
   return (
     <li>
-      {!editing && (
-        <>
-          <label>
-            <input
-              type="checkbox"
-              checked={completed}
-              onChange={(e) => toggleTodo(id, e.target.checked)}
-            />
-            {title}
-          </label>
-          <button onClick={() => deleteTodo(id)} className="btn btn-danger">
-            Delete
-          </button>
-          <button onClick={() => setEditing(true)} className="btn btn-primary">
-            Edit
-          </button>
-        </>
-      )}
-      {editing && (
-        <>
-          <input
-            value={newTitle}
-            onChange={(e) => setNewTitle(e.target.value)}
-            type="text"
-          />
-          <button onClick={handleSave} className="btn btn-success">
-            Save
-          </button>
-          <button onClick={() => setEditing(false)} className="btn btn-secondary">
-            Cancel
-          </button>
-        </>
-      )}
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={e => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        Delete
+      </button>
     </li>
   )
 }
